cd96cc7ab56d0592968b368d484e6fde114655be
nanggi4 <nkhun1006@gmail.com>
2021-12-02 23:41:01 +0900

    section3 update

--- Section 3/Parsing Request Paths/index.js
+++ Section 3/Parsing Request Paths/index.js
@@ -4,27 +4,24 @@
  */
 
 // Dependencies
-var http = require('http');
-var url = require('url');
+const http = require('http');
+const url = require('url');
 
- // Configure the server to respond to all requests with a string
-var server = http.createServer(function(req,res){
+const PORT = 3000;
 
-  // Parse the url
-  var parsedUrl = url.parse(req.url, true);
+const server = http.createServer((req, res) => {
+
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
 
   // Get the path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const path = parsedUrl.pathname;
 
   // Send the response
-  res.end('Hello World!\n');
-
-  // Log the request/response
-  console.log('Request received on path: '+trimmedPath);
+   res.end('Hello World\n');
+  // Log the request path
 });
 
-// Start the server
-server.listen(3000,function(){
-  console.log('The server is up and running now');
-});
+server.listen(PORT, () => {
+  console.log(`The server is listening on port ${PORT} now`);
+})
