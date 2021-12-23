112f41b0b333f5d9da7d170be7125ca38b29fc35
nanggi4 <nkhun1006@gmail.com>
2021-12-24 00:21:08 +0900

    training update

--- Section 3/Training/index.js
+++ Section 3/Training/index.js
@@ -4,10 +4,34 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+
+  // Get the URL and parse it
+  let parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
+  
+  // Send the reponse
   res.end('Hello World\n');
+
+  // Log the request path 
+  console.log('Request received on path: ' + trimmedPath 
+  + ' with method: ' + method + ' and with these query string parameter ' 
+  + queryStringObject);
 });
 
 server.listen(PORT, () => {
